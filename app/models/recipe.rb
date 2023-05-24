638e7c6d747c671949cfcdeb290c998cdfa4f168
NelsonAruajo93 <nelsonaraujoparedes93@hotmail.com>
2023-05-24 08:36:07 -0500

    Fix linters in /models/recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   has_many :recipes_foods
 
   validates :name, presence: true, length: { maximum: 250 }
-  validates :description,  presence: true, length: { maximum: 250 }
-  validates :cooking_time,  presence: true, comparison: { greater_than: 0 }
-  validates :preparation_time,  presence: true, comparison: { greater_than: 0 }
+  validates :description, presence: true, length: { maximum: 250 }
+  validates :cooking_time, presence: true, comparison: { greater_than: 0 }
+  validates :preparation_time, presence: true, comparison: { greater_than: 0 }
 end
